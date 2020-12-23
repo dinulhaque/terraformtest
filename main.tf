e7115a49ee7bc470f77dc191b114df04d1ca5bd1
dinulhaque <dinul-haque@live.co.uk>
2020-12-23 21:56:56 +0000

    create test file

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "eu-west-2"
+  
+}
+
+
+resource "aws_instance" "test" {
+ami = "ami-04de2b60dd25fbb2e"
+instance_type = "t2.micro"
+tags {
+  Name = "TF Test"
+}
+
+  
+}
